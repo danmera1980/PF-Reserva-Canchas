599d945bcd6e691506ff32f95dc64ba5069df7c2
luisbasmar <luis.basmar20@gmail.com>
2022-01-28 14:30:31 -0500

    Branch Test

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -1,26 +1,24 @@
-const axios = require("axios");
-const {User} = require('../db');
+/** @format */
 
+const axios = require('axios');
+const { User } = require('../db');
 
+//test branch
 const getAllUsers = async (req, res, next) => {
-    try {
-        
-    } 
-    catch (e) {
-        next(e)
-    }
+  try {
+  } catch (e) {
+    next(e);
+  }
 };
 
-
 const getUserByID = async (req, res, next) => {
-    try {
-        
-    } catch (e) {
-        next(e)
-    }
+  try {
+  } catch (e) {
+    next(e);
+  }
 };
 
 module.exports = {
-    getAllUsers,
-    getUserByID
-}
+  getAllUsers,
+  getUserByID,
+};
