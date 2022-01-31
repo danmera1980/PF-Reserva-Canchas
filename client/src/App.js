14c1c9f8c2e5888af4be924a59ba8c0f1d6a1620
danmera1980 <danmera1980@gmail.com>
2022-01-31 11:43:34 -0500

    New home-filters branch and files created

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import "../src/styles/index.scss";
 import { Route } from "react-router-dom";
-import Landing from "./components/Landing/Landing";
+import Home from "./components/Home/Home";
 import React from "react";
 
 function App() {
   return (
     <React.Fragment>
       <div className="App">
-        <Route exact path="/" component={Landing} />
+        <Route exact path="/" component={Home} />
       </div>
     </React.Fragment>
   );
