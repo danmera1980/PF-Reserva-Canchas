726bb5d54732fd977769126ac99f4f71cf7075c2
bianchinmazzer <matias.bianchin@gmail.com>
2022-02-01 01:22:56 -0300

    Componente Crear establecimiento y validaciones(les falta  mejorar) y ruta a ?establishment

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Route } from "react-router-dom";
 import Landing from "./components/Landing/Landing";
 import React from "react";
 import CourtCreate from "./components/CourtCreate/CourtCreate";
+import PostEstablishment from "./components/PostEstablishment/PostEstablishment";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <Route exact path="/" component={Landing} />
         <Route exact path="/court" component={CourtCreate} />
+        <Route exact path="/establishment" component={PostEstablishment} />
       </div>
     </React.Fragment>
   );
--- client/src/components/PostEstablishment/PostEstablishment.jsx
+++ client/src/components/PostEstablishment/PostEstablishment.jsx
@@ -3,9 +3,31 @@ import { Link, useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { postEstablishment } from "../../redux/actions";
 
+function validate(input) {
+ 
+    let errors = {};
+    if(input.cuit === '') {
+        errors.cuit = "Se requiere un cuit"
+    }
+    if(input.name === '') {
+        errors.name = "Se requiere un nombre de establecimiento"
+    } 
+    if(input.timeActiveFrom === '') {
+        errors.timeActiveFrom = "Se requiere un horario de apertura"
+    }
+    if (input.timeActiveTo === ''){
+        errors.timeActiveTo = "Se requiere un horario de cierre"
+    }
+    if(input.responsable_id === '') {
+        errors.responsable_id = "Se requiere un id del usuario responsable"
+    }
+    return errors
+}
+
 export default function PostEstablishment() {
     const dispatch = useDispatch()
     const history = useHistory()
+    const [errors, setErrors] = useState({});
     const [input, setInput] = useState({
         cuit: null,
         name: "",
@@ -21,6 +43,10 @@ export default function PostEstablishment() {
             ...input,
             [e.target.name] : e.target.value
         })
+        setErrors(validate({
+            ...input,
+            [e.target.name]: e.target.value
+        }))
     }
     function handleSubmit(e) {
         e.preventDefault()
@@ -45,10 +71,16 @@ export default function PostEstablishment() {
                     <div>
                     <label>Cuit: </label>
                     <input placeholder="Cuit..." type="text" value={input.cuit} name="cuit" onChange={(e)=>handleChange(e)}></input>
+                    {errors.cuit ?
+                    <p>{errors.cuit}</p> : null
+                    }
                     </div>
                     <div>
                     <label>Nombre: </label>
                     <input placeholder="Nombre..." type="text" value={input.name} name="name" onChange={(e)=>handleChange(e)}></input>
+                    {errors.name ?
+                    <p>{errors.name}</p> : null
+                    }
                     </div>
                     <div>
                     <label>Descripcion: </label>
@@ -66,13 +98,22 @@ export default function PostEstablishment() {
                     <label>Horario de apertura: </label>
                     <input placeholder="Hora de apertura..." type="text" value={input.timeActiveFrom} name="timeActiveFrom" onChange={(e)=>handleChange(e)}></input>
                     </div>
+                    {errors.timeActiveFrom ?
+                    <p>{errors.timeActiveFrom}</p> : null
+                    }
                     <div>
                     <label>Horario de cierre: </label>
                     <input placeholder="Hora de cierre..." type="text" value={input.timeActiveTo} name="timeActiveTo" onChange={(e)=>handleChange(e)}></input>
                     </div>
+                    {errors.timeActiveTo ?
+                    <p>{errors.timeActiveTo}</p> : null
+                    }
                     <div>
                     <label>Numero de usuario responsable: </label>
                     <input placeholder="Numero(id) del usuario responsable..." type="text" value={input.responsable_id} name="responsable_id" onChange={(e)=>handleChange(e)}></input>
+                    {errors.responsable_id ?
+                    <p>{errors.responsable_id}</p> : null
+                    }
                     </div>
                     <button type="submit">Crear Establecimiento</button> 
                     <br/>
