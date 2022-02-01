72f230a50f114bbec6a7313244741d111dc6f99a
Franco Visintini <francovisintini@gmail.com>
2022-02-01 17:43:12 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,12 @@
 const { Router } = require('express');
+const router = Router();
+
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
 const routerEstablishment = require('./routerEstablishment');
 const routerSite = require('./routerSite');
 const userRoute = require("./user");
 const Court = require("./court");
-
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
-const router = Router();
 
 router.use('/establishment', routerEstablishment)
 router.use('/site', routerSite)
