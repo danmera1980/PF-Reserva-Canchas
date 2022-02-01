1883dae552d6f0445e2dce62318b1ac1b7793923
Federico Voloschin <fedevoloschin@gmail.com>
2022-02-01 18:57:07 -0300

    Cartas con la info del back

--- api/src/controllers/card.js
+++ api/src/controllers/card.js
@@ -1,4 +1,4 @@
-const { Court, Site, Establishment} = require ('../db');
+const { Court, Site} = require ('../db');
 
 let courtDB = ()=> {
     return  Court.findAll()
@@ -6,40 +6,27 @@ let courtDB = ()=> {
 let siteDB = ()=> {
     return  Site.findAll()
 }
-let establishmentDB = ()=> {
-    return  Establishment.findAll()
-}
-
 
 const getCards = async (req,res)=>{
 
     const allCourts = await courtDB();
     const allSites = await siteDB();
-    const allEstablishments = establishmentDB();
-    const infoCourts=allCourts.map(el=>{return {name:el.name, price:el.price}})
-    const infoSites=allSites.map(el=>{return {siteName:el.name,city:el.city, street:el.street, streetNumber:el.streetNumber}})
+   
     var infoCard=[]
 
     for (let i = 0; i < allCourts.length; i++) {
-        for (let j = 0; i < allSites.length; i++) {
+        for (let j = 0; j < allSites.length; j++) {
              
                 if (allCourts[i].siteName===allSites[j].name){
-                    infoCard=[...infoCard,[ {name:allCourts[i].name, siteName:allSites[j].name, city:allSites[j].city,shiftLength:allCourts[i].shiftLength,
-                    price:allCourts[i].price, street:allSites[j].street, streetNumber:allSites[j].streetNumber}]]   
+                var infoCardIn={name:allCourts[i].name, siteName:allSites[j].name, city:allSites[j].city,shiftLength:allCourts[i].shiftLength,
+                    price:allCourts[i].price, street:allSites[j].street, streetNumber:allSites[j].streetNumber}
             }
-            
-        
+            infoCard=[...infoCard,infoCardIn]
+    
         }
-        
     }
-    //  if(allCourts.map(el=>el.siteName)===allSites.map(el=>el.name)){
-    //   var definitivo=infoCourts.concat(infoSites)
-    // }
-    console.log(infoCard)
+   
     try {
-
-     
-        const cards = allCourts.map(el=>{return {name:el.name, price:el.price}})
 
        res.status(200).send(infoCard)    
         
