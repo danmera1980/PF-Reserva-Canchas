8f34c6325880d64bfab7e9dc6749a15645cb6fde
danmera1980 <danmera1980@gmail.com>
2022-02-02 18:43:10 -0500

    fix(searchbar css) fixed some classes fix(app remove)  removed login code

--- client/src/App.js
+++ client/src/App.js
@@ -14,36 +14,9 @@ import SiteCreate from "./components/SiteCreate/SiteCreate";
 
 function App() {
 
-  const [user, setUser] = useState(null);
-  useEffect(() => {
-    const getUser = () => {
-      fetch('http://localhost:5000/auth/login/success', {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Credentials': true,
-        },
-      })
-        .then(response => {
-          if (response.status === 200) return response.json();
-          throw new Error('authentication has been failed!');
-        })
-        .then(resObject => {
-          setUser(resObject.user);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    };
-    getUser();
-  }, []);
-  
   return (
     <React.Fragment>
       <div className="App">
-        <Navbar user={user} />
         <Route exact path="/" component={Home} />
         <Route exact path="/court" component={CourtCreate} />
         <Route exact path="/results" component={Results} />
