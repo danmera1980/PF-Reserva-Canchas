d83141d30565ac74bc2e68d9b598e15d4e651483
beckman924 <braiancarabajal@gmail.com>
2022-02-03 12:54:19 -0300

    some changes to Profile component

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -6,30 +6,31 @@ function Profile() {
   return (
     <div>
       <Header />
-      <div className="w-full pb-32 bg-[#F4B30B]"></div>
-      <div className="">
-        <img
-          src={logo}
-          alt="logo_img"
-          className="bg-cover rounded-full w-60 h-60 bg-green-900  z-10 -mt-20 ml-32"
-        />
-      </div>
+      <div className="md:w-[1200px] m-auto">
+        <div className=" h-36 bg-[#F4B30B]"></div>
+        <div className="-mt-28 ml-20">
+          <img
+            src={logo}
+            alt="logo_img"
+            className="bg-cover rounded-full w-60 h-60 bg-green-900 relative"
+          />
+        </div>
 
-      <div>
-        <h1 className="text-center mt-5 text-2xl font-bold">
-          Establishment Name
-        </h1>
-        <button className="pt-2">Edit photo</button>
-      </div>
+        <div>
+          <h1 className="text-center mt-5 text-2xl font-bold">
+            Nombre de usuario
+          </h1>
+        </div>
 
-      <div className="mt-10">
-        <h1 className="text-xl font-bold">Mis Reservas</h1>
-        <ol className="mt-5 grid grid-cols-3 underline">
-          <li className="cursor-pointer">Actuales</li>
-          <li className="cursor-pointer">Pasadas</li>
-          <li className="cursor-pointer">Canceladas</li>
-        </ol>
-        <div className="grid row-span-full"></div>
+        <div className="mt-10">
+          <h1 className="text-xl font-bold">Mis Reservas</h1>
+          <ol className="mt-5 grid grid-cols-3 underline">
+            <li className="cursor-pointer">Actuales</li>
+            <li className="cursor-pointer">Pasadas</li>
+            <li className="cursor-pointer">Canceladas</li>
+          </ol>
+          <div className="grid row-span-full"></div>
+        </div>
       </div>
     </div>
   );
