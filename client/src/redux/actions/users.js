452c935dfaabe70ac808bc3610cf387a6b45ea56
Ale1422 <alejandro.c.14.22@gmail.com>
2022-02-03 17:25:03 -0300

    componente userEdit, actions,reducers

--- client/src/redux/actions/users.js
+++ client/src/redux/actions/users.js
@@ -5,7 +5,7 @@
  */
 
 import axios from 'axios';
-import { ALL_USERS, REGISTER, LOGIN, LOGINGOOGLE } from './actionNames';
+import { ALL_USERS, REGISTER, LOGIN, LOGINGOOGLE, EDIT_SUCCESS, SET_ERRORS } from './actionNames';
 const serverUrl = 'localhost';
 
 export const getAllUsers = () => {
@@ -49,4 +49,23 @@ export function loginWithGoogle(payload) {
         return dispatch({ type: LOGINGOOGLE, payload: payload });
       }
       
+}
+
+export function editUser(payload, userToken) {
+  const headers = {  
+     'Authorization': `Bearer ${userToken}`
+   }
+   
+  return  async function (dispatch){
+    try {
+      const response = await axios.put(
+        `http://${serverUrl}:3001/users/login`,
+         payload,
+         {headers:headers}
+         )
+      return dispatch({type: EDIT_SUCCESS, payload: response.data})        
+    } catch (error) {
+        return dispatch({type: SET_ERRORS, payload: error})
+    }
+   }
 }
