dff3c4971c6d97b1666a41ce8eef53d530bc2cb6
beckman924 <braiancarabajal@gmail.com>
2022-02-04 00:22:44 -0300

    Grid fix

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -3,6 +3,8 @@ import logo from "../../assets/img/logo.png";
 import Header from "../Header/Header";
 import Card from "../Card/Card";
 import Footer from "../Footer/Footer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCog, faFutbol, faMoneyCheckAlt, faThLarge } from "@fortawesome/free-solid-svg-icons";
 
 function Profile() {
   return (
@@ -10,39 +12,60 @@ function Profile() {
       <Header />
       <div className="md:max-w-[1200px] m-auto">
         <div className=" h-36 bg-[#F4B30B]"></div>
-        <div className="-mt-28 ml-16 md:ml-20">
+        <div className="md:grid md:grid-cols-2 xl:grid-cols-[30%,70%] h-3/4">
+          <div>
           <img
             src={logo}
             alt="logo_img"
-            className="bg-cover rounded-full w-60 h-60 bg-green-900 relative"
+            className="-mt-28 ml-16 md:ml-20 bg-cover rounded-full w-60 h-60 bg-green-900 relative"
           />
-        </div>
-        <h1 className=" mb-5 text-center mt-5 text-2xl font-bold md:text-left md:ml-24 md:inline-block">
-          Nombre de usuario
-        </h1>
+          
+          <h1 className=" mb-5 text-center mt-5 text-2xl font-bold md:text-left md:ml-24 md:inline-block">
+            Nombre de usuario
+          </h1>
+          
 
-        <div className="md:grid md:grid-cols-2">
-          <div className="grid grid-cols-2 gap-4 ml-5 md:ml-7 max-w-xs">
-            <button className="rounded-lg shadow-xl py-3 md:py-2 bg-white text-black ">
-              Mis Reservas
-            </button>
-            <button className="rounded-lg shadow-xl py-3 md:py-2 bg-white text-black ">
-              Transacciones
-            </button>
-            <button className="rounded-lg shadow-xl py-3 md:py-2 bg-white text-black ">
-              Establecimiento
-            </button>
-            <button className="rounded-lg shadow-xl py-3 md:py-2 bg-white text-black ">
-              Editar perfil
-            </button>
-          </div>
 
-          <div className="pt-7 md:pt-0">
-            <Card />
+          <div className="md:grid md:grid-cols-2 md:w-max">
+            <div className="grid grid-cols-2 gap-4 ml-5 md:ml-7 max-w-xs">
+              <button className="rounded-lg shadow-xl py-3 md:py-2 bg-white text-black active:scale-95 transition-all">
+                <FontAwesomeIcon icon={faThLarge} size={"2x"}/>
+                <p>
+                Mis Reservas
+                </p>
+              </button>
+              <button className="rounded-lg shadow-xl py-3 md:py-2 bg-white text-black active:scale-95 transition-all">
+                <FontAwesomeIcon icon={faMoneyCheckAlt} size={"2x"}/>
+                <p>
+                Transacciones
+                </p>
+              </button>
+              <button className="rounded-lg shadow-xl py-3 md:py-2 bg-white text-black active:scale-95 transition-all">
+                <FontAwesomeIcon icon={faFutbol} size={"2x"}/>
+                <p>
+                Establecimiento
+                </p>
+              </button>
+              <button className="rounded-lg shadow-xl py-3 md:py-2 bg-white text-black active:scale-95 transition-all">
+                <FontAwesomeIcon icon={faCog} size={"2x"}/>
+                <p>
+                Editar perfil
+                </p>
+              </button>
+            </div>
+            </div>
+
+
           </div>
+            <div className="pt-7 md:overflow-auto md:max-h-fit">
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+            </div>
         </div>
       </div>
-      <div className="md:absolute py-1 px-1 bottom-0 left-0 right-0">
+      <div className="bottom-0 left-0 right-0">
         <Footer />
       </div>
     </div>
