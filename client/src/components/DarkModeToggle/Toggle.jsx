b3c6376e915960836d52cdab60bf02b51ee46144
beckman924 <braiancarabajal@gmail.com>
2022-02-05 00:12:43 -0300

    fixing Toggle

--- client/src/components/DarkModeToggle/Toggle.jsx
+++ client/src/components/DarkModeToggle/Toggle.jsx
@@ -4,7 +4,7 @@ function Toggle() {
   return (
     <div className="w-28 h-12 rounded-[200px] border-2 overflow-hidden relative bg-[url('https://leerzaam.com/wp-content/uploads/2021/03/10526.jpg')] bg-center bg-cover shadow-lg shadow-white">
         <input type="checkbox" className="peer transition-all hidden"/>
-        <label className="absolute z-10 w-[2.1rem] h-[2.1rem] mt-[5px] ml-[7px] rounded-[50%] bg-white cursor-pointer peer-checked:translate-x-10 transition-all"></label>
+        <label className="absolute z-10 w-[2.1rem] h-[2.1rem] mt-[5px] ml-[7px] rounded-[50%] bg-white cursor-pointer peer-checked:translate-x-10 transition-all "></label>
     </div>
   );
 }
