388c825e11292476f5e442fd92be43a371b361b2
bianchinmazzer <matias.bianchin@gmail.com>
2022-02-05 17:57:57 -0300

    componente perfil establecimiento

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Register from "./components/Register/Register";
 import SiteCreate from "./components/SiteCreate/SiteCreate";
 import Profile from "./components/Profile/Profile";
 import UserEdit from './components/UserEdit/UserEdit';
+import EstablishmentProfile from "./components/EstablishmentProfile/EstablishmentProfile";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route exact path="/register" component={Register} />
         <Route exact path="/profile" component={Profile} />
         <Route exact path="/useredit" component={UserEdit} />
+        <Route exact path="/establishmentprofile" component={EstablishmentProfile} />
       </div>
     </React.Fragment>
   );
