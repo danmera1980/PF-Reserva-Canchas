503338b9a077046c08c6170b81a92db1c815e9e7
bianchinmazzer <matias.bianchin@gmail.com>
2022-02-06 13:50:59 -0300

    perfil establecimiento

--- client/src/components/EstablishmentProfile/EstablishmentProfile.jsx
+++ client/src/components/EstablishmentProfile/EstablishmentProfile.jsx
@@ -1,22 +1,44 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import logo from "../../assets/img/logo.png";
 
 
 
 function EstablishmentProfile() {
     return (
         <div>
-            <div>
-                <Link to={"/site"}>
-                    <button>Crear sede</button>
-                </Link>
-            </div>
-            <div>
-                <Link to={"/court"}>
-                    <button>Crear cancha</button>
-                </Link>
-            </div>
+      <Header />
+      <div className="md:max-w-[1200px] m-auto">
+        <div className="h-36 bg-[#F4B30B]"></div>
+        <div className="md:grid md:grid-cols-2 xl:grid-cols-[30%,70%] h-3/4">
+          <div>
+            <img
+              src={logo}
+              alt="logo_img"
+              className="-mt-28 ml-16 md:ml-20 bg-cover rounded-full w-60 h-60 bg-green-900 relative"
+            />
+            <div className="flex justify-center items-center mt-5">
+            <div>          
+                    <Link to={"/site"}>
+                        <button className="ml-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded shadow-2xl shadow-indigo-600">Crear sede</button>
+                    </Link>
+                </div>
+                <div>
+                    <Link to={"/court"}>
+                        <button className="ml-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded shadow-2xl shadow-indigo-600">Crear cancha</button>
+                    </Link>
+                </div>
+                </div>
+          </div>
+         
         </div>
+      </div>
+      <div className="mb-0 mt-80">
+        <Footer />
+      </div>
+    </div>
     )
 }
 
