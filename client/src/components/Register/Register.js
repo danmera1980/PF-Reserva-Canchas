a729cc2efa9e65f12a8037415fd8111bca3e0283
bianchinmazzer <matias.bianchin@gmail.com>
2022-02-06 02:03:40 -0300

    formularios css

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -54,85 +54,130 @@ const Register = () => {
   };
 
   return (
-    <div className="">
-      <h1 className="">Registrate</h1>
+    <div >
+      <h1 className="flex justify-center text-xl text-indigo-800">Registrate</h1>
+      <br/>
       <div className="">
-        <div className="">
-          <div className="" onClick={google}>
-            <img src={Google} alt="" />
+        <div className="flex justify-center cursor-pointer">
+          <div className="flex justify-center text-green-400 bg-slate-500 h-10 w-24 items-center rounded-md" onClick={google}>
+            <img className="" src={Google} alt="" />
             Google
           </div>
         </div>
-        <div className="">
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label>Nombre: </label>
-              <input
+        <div className="flex justify-center">
+          <form className="md:w-3/5 lg:w-2/5 flex-col justify-center items-center mx-5 border-grey-400 border-2 mt-10 bg-white drop-shadow-md backdrop-blur-3xl rounded-md px-3 py-3" onSubmit={handleSubmit}>
+            <div className="relative ">
+              <input className="w-full peer placeholder-transparent h-10   border-b-2 border-grey-300 focus:outline-none focus:border-indigo-600 bg-transparent"
                 type="text"
                 name="name"
                 value={userInfo.name}
                 placeholder="Escribe tu nombre"
                 autoComplete="off"
+                id='nombre' 
                 onChange={handleChange}
-              ></input>
+                ></input>
+                <label className="absolute left-0 -top-3.5 
+                                            text-gray-600 text-sm 
+                                            peer-placeholder-shown:text-base 
+                                            peer-placeholder-shown:text-gray-400
+                                            peer-placeholder-shown:top-2 transition-all 
+                                            peer-focus:-top-3.5 peer-focus:text-gray-600
+                                            peer-focus:text-sm
+                                            cursor-text" htmlFor="nombre">Nombre </label>
             </div>
-            {errors.name && <p>{errors.name}</p>}
-            <div>
-              <label>Apellido: </label>
+            {errors.name && <p className="text-xs text-red-500">{errors.name}</p>}
+            <div className="relative mt-3">
               <input
+                className="w-full peer placeholder-transparent h-10   border-b-2 border-grey-300 focus:outline-none focus:border-indigo-600 bg-transparent"
                 type="text"
                 name="lastName"
                 value={userInfo.lastName}
                 placeholder="Escribe tu Apellido"
                 autoComplete="off"
+                id="apellido"
                 onChange={handleChange}
-              ></input>
+                ></input>
+                <label className="absolute left-0 -top-3.5 
+                                            text-gray-600 text-sm 
+                                            peer-placeholder-shown:text-base 
+                                            peer-placeholder-shown:text-gray-400
+                                            peer-placeholder-shown:top-2 transition-all 
+                                            peer-focus:-top-3.5 peer-focus:text-gray-600
+                                            peer-focus:text-sm
+                                            cursor-text" htmlFor="apellido">Apellido </label>
             </div>
-            {errors.lastName && <p>{errors.lastName}</p>}
+            {errors.lastName && <p className="text-xs text-red-500">{errors.lastName}</p>}
             
-            <div>
-              <label>Email: </label>
+            <div className="relative mt-3">
               <input
+              className="w-full peer placeholder-transparent h-10   border-b-2 border-grey-300 focus:outline-none focus:border-indigo-600 bg-transparent"
                 type="email"
                 name="email"
                 value={userInfo.email}
                 placeholder="Escribe tu Email"
                 autoComplete="off"
+                id="mail"
                 onChange={handleChange}
-              ></input>
+                ></input>
+                <label className="absolute left-0 -top-3.5 
+                                            text-gray-600 text-sm 
+                                            peer-placeholder-shown:text-base 
+                                            peer-placeholder-shown:text-gray-400
+                                            peer-placeholder-shown:top-2 transition-all 
+                                            peer-focus:-top-3.5 peer-focus:text-gray-600
+                                            peer-focus:text-sm
+                                            cursor-text" htmlFor="mail">Email </label>
             </div>
-            {errors.email && <p>{errors.email}</p>}
-            <div>
-              <label>Contraseña: </label>
+            {errors.email && <p className="text-xs text-red-500">{errors.email}</p>}
+            <div className="relative mt-3">
               <input
+                className="w-full peer placeholder-transparent h-10   border-b-2 border-grey-300 focus:outline-none focus:border-indigo-600 bg-transparent"
                 type="password"
                 name="password"
                 value={userInfo.password}
                 placeholder="Escribe tu contraseña"
                 autoComplete="off"
+                id="pw"
                 onChange={handleChange}
-              ></input>
+                ></input>
+                <label className="absolute left-0 -top-3.5 
+                                            text-gray-600 text-sm 
+                                            peer-placeholder-shown:text-base 
+                                            peer-placeholder-shown:text-gray-400
+                                            peer-placeholder-shown:top-2 transition-all 
+                                            peer-focus:-top-3.5 peer-focus:text-gray-600
+                                            peer-focus:text-sm
+                                            cursor-text" htmlFor="pw">Contraseña </label>
             </div>
-            {errors.password && <p>{errors.password}</p>}
-            <div>
-              <label>Confirma la Contraseña: </label>
+            {errors.password && <p className="text-xs text-red-500">{errors.password}</p>}
+            <div className="relative mt-3">
               <input
+                className="w-full peer placeholder-transparent h-10   border-b-2 border-grey-300 focus:outline-none focus:border-indigo-600 bg-transparent"
                 type="password"
                 name="confirmPassword"
                 value={userInfo.confirmPassword}
                 placeholder="Confirma la contraseña"
                 autoComplete="off"
+                id="pw2"
                 onChange={handleChange}
-              ></input>
+                ></input>
+                <label className="absolute left-0 -top-3.5 
+                                            text-gray-600 text-sm 
+                                            peer-placeholder-shown:text-base 
+                                            peer-placeholder-shown:text-gray-400
+                                            peer-placeholder-shown:top-2 transition-all 
+                                            peer-focus:-top-3.5 peer-focus:text-gray-600
+                                            peer-focus:text-sm
+                                            cursor-text" htmlFor="pw2">Confirma la Contraseña </label>
             </div>
-            {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+            {errors.confirmPassword && <p className="text-xs text-red-500">{errors.confirmPassword}</p>}
                      
 
             <div>
-              <button type="submit">Registrarse</button>
+              <button className="mt-5 w-full bg-indigo-400 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Registrarse</button>
             </div>
             <div>
-              <p>
+              <p className="mt-3 text-md text-indigo-600">
                 ¿Ya estas registrado? <Link to="/login">Ingresa aquí</Link>
               </p>
             </div>
