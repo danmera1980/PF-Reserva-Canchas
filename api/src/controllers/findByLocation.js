2f76c15b80be57533f3f045d093db73bf3a60c3b
Ale1422 <alejandro.c.14.22@gmail.com>
2022-02-08 19:02:34 -0300

    Filtro ubicacion

--- api/src/controllers/findByLocation.js
+++ api/src/controllers/findByLocation.js
@@ -1,7 +1,11 @@
 const { Court, Site, Establishment, Op} = require ('../db');
 
 const findByLocation = async (req, res) => { 
-    const {location} = req.query
+    var {latitude, longitude, sport, zoom } = req.query
+    // lat: 0.273323
+    // lng: 0.150562
+    const amplLat =  0.273323
+    const amplLng =  0.150562
 
     try {
       var establishments = await Establishment.findAll({
@@ -14,9 +18,16 @@ const findByLocation = async (req, res) => {
           }
         },
         where:{
-          '$sites.latitude$': {
-            [Op.lte]: location
-          },
+          [Op.and]: [
+            {'$sites.latitude$': {[Op.lte]: latitude + amplLat }},
+            {'$sites.latitude$': {[Op.gte]: latitude - amplLat}}
+          ],
+          [Op.and]: [
+            {'$sites.longitude$': {[Op.lte]: longitude + amplLng }},
+            {'$sites.longitude$': {[Op.gte]: longitude - amplLng }}
+          ]
+          // '$sites.latitude$': {[Op.lte]: latitude},
+          // '$sites.longitude$': { [Op.gt]: longitude}
         }
       })
       res.send(establishments)
