549e0c7fdb8ff96a1ec263c2277c051455332cae
bianchinmazzer <matias.bianchin@gmail.com>
2022-02-11 11:23:12 -0300

    css y horas

--- client/src/components/Calendar/Hours.jsx
+++ client/src/components/Calendar/Hours.jsx
@@ -4,8 +4,14 @@ const Hours = () => {
     var horas = Array.from(Array(24).keys())
     return (
         horas.map((h)=> (
-            <div>
-                {h}:00
+            <div className="bg-white inline-table">
+            <div className="box-border container grid grid-cols-4 grid-rows-6 gap-3">
+                <div className="box-border h-10 w-14 p-4 border-4 flex justify-center items-center">
+                    <ul>
+                        {h}:00
+                    </ul>
+                </div>
+            </div>
             </div>
         ))
     )
