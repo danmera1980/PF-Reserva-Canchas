127c532b2091b6f753471438385f3877093af4b4
bianchinmazzer <matias.bianchin@gmail.com>
2022-02-16 14:57:28 -0300

    tabla usuarios

--- client/src/components/AdminProfile/AdminProfile.jsx
+++ client/src/components/AdminProfile/AdminProfile.jsx
@@ -7,47 +7,50 @@ import UsersTable from "./UsersTable";
 import { useSelector } from "react-redux";
 
 function AdminProfile() {
-const [users, setUsers] = useState(null);
-const [visual, setVisual] = useState("");
-const userToken = useSelector((state) => state.register.userToken);
+  const [users, setUsers] = useState(null);
+  const [visual, setVisual] = useState("");
+  const userToken = useSelector((state) => state.register.userToken);
 
-const onButtonSelection = (option) => {
+  const onButtonSelection = (option) => {
     setVisual(option);
-    };
+  };
 
-    useEffect(()=>{
-        const headers = {
-            Authorization: `Bearer ${userToken}`,
-          };
-       axios.get(`${SERVER_URL}/users` , { headers: headers })
-       .then((res)=> {
-           setUsers(res.data)
-       })
-        }, [userToken])     
-    
-      return (
-        <div>
-            <Header/>
-                <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold border border-blue-700 rounded shadow-2xl shadow-indigo-600 active:scale-95 transition-all h-[4.5rem]"
-                    onClick={() => onButtonSelection("users")}
-                    >
-                    Ver usuarios
-                </button>
-                    <div>
-                    {(() => {
-                        switch (visual) {
-                        case "users":
-                        return <UsersTable users={users}/>
-                        default:
-                        return <h1>Shordana</h1>    
-                         }
-                         
-                            })()}
-                    </div>
-            <Footer/>
-        </div>
-    )
+  useEffect(() => {
+    const headers = {
+      Authorization: `Bearer ${userToken}`,
+    };
+    axios.get(`${SERVER_URL}/users`, { headers: headers }).then((res) => {
+      setUsers(res.data);
+    });
+  }, [userToken]);
+
+  return (
+    <div>
+      <Header />
+      <button
+        className="mt-5 ml-40 h-10 bg-blue-500 hover:bg-blue-700 text-white font-bold border border-blue-700 rounded shadow-2xl shadow-indigo-600 active:scale-95 transition-all "
+        onClick={() => onButtonSelection("users")}
+      >
+        Ver usuarios
+      </button>
+
+      <div>
+        {(() => {
+          switch (visual) {
+            case "users":
+              return <UsersTable users={users} />;
+            default:
+              return (
+                <h1 className="text-indigo-500 flex justify-center text-2xl">
+                  Perfil Administrador
+                </h1>
+              );
+          }
+        })()}
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default AdminProfile;
--- client/src/components/AdminProfile/UsersTable.jsx
+++ client/src/components/AdminProfile/UsersTable.jsx
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function UsersTable({ users }) {
+  const [usersTemp, setUsersTemp] = useState(users);
+
+  useEffect(() => {}, [usersTemp]);
+
+  function handleFilter(e) {
+    if (e.target.value !== "all") {
+      if (e.target.value === "true") {
+        setUsersTemp(users.filter((u) => u.hasEstablishment === true));
+      } else {
+        setUsersTemp(users.filter((u) => u.hasEstablishment === false));
+      }
+    } else {
+      setUsersTemp(users);
+    }
+    console.log(usersTemp, e.target.value);
+  }
   return (
     <div>
-      <table>
-        <thead>
+      <table className="ml-36 mt-10 table-auto border-collapse border border-slate-500 text-white">
+        <thead className="bg-slate-600">
           <tr>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Telefono</th>
-            <th>Email</th>
-            <th>Posee Establecimiento</th>
+            <th className="border border-slate-600 px-10">Nombre</th>
+            <th className="border border-slate-600 px-10">Apellido</th>
+            <th className="border border-slate-600 px-10">Email</th>
+            <th className="border border-slate-600 px-10">Telefono</th>
+            <th className="border border-slate-600 px-10">
+              Posee Establecimiento
+              <select
+                className="text-black border-2 rounded-md"
+                onChange={(e) => handleFilter(e)}
+              >
+                <option value="all">Todos</option>
+                <option value="true">Con establecimiento</option>
+                <option value="false">Sin establecimiento</option>
+              </select>
+            </th>
+            <th className="border border-slate-600 px-10">
+              Habilitar/Deshabilitar
+            </th>
           </tr>
         </thead>
-        <tbody>
-          {users.map((u) => (
-            <tr>
-              <td>{u.name}</td>
-              <td>{u.lastName}</td>
-              <td>{u.email}</td>
-              <td>{u.phone}</td>
-              <td>{u.hasEstablishment ? "si" : "no"}</td>
+        <tbody className="text-center text-white">
+          {usersTemp?.map((u) => (
+            <tr key={u.id}>
+              <td className="border border-slate-700">{u.name}</td>
+              <td className="border border-slate-700">{u.lastName}</td>
+              <td className="border border-slate-700">{u.email}</td>
+              <td className="border border-slate-700">{u.phone}</td>
+              <td className="border border-slate-700">
+                {u.hasEstablishment ? "si" : "no"}
+              </td>
+              <td className="border border-slate-700">
+                <input type="checkbox"></input>
+              </td>
             </tr>
           ))}
         </tbody>
