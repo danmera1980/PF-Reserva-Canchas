0ffc04609894da0a5f4ab7354e82ccdf7edff2c0
danmera1980 <danmera1980@gmail.com>
2022-02-17 17:23:15 -0500

    schedule library and component added

--- client/src/components/EstablishmentBookings/EstablishmentBookings.jsx
+++ client/src/components/EstablishmentBookings/EstablishmentBookings.jsx
@@ -1,13 +1,19 @@
 import axios from "axios";
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import Paper from '@mui/material/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
 import { SERVER_URL } from "../../redux/actions/actionNames";
 
 function EstablishmentBookings({establishmentDetail}) {
   const [courts, setCourts] = useState(null);
   // setCourts(establishmentDetail.map(e => e.courts.map(e => e.id)))
   console.log(establishmentDetail)
+  var currentDate = Date.now()
 
 
   // useEffect(() => {
@@ -16,6 +22,21 @@ function EstablishmentBookings({establishmentDetail}) {
 
   return <div>
       <h1>Reservas</h1>
+      <div>
+        {/* <Paper>
+          <Scheduler
+            data={{establishmentDetail}}
+          />
+          <ViewState 
+            currentDate={currentDate}
+          />
+          <DayView
+            startDayHour={9}
+            endDayHour={14}
+          />
+        </Paper> */}
+      </div>
+
   </div>;
 }
 
