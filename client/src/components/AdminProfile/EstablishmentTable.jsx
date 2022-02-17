60bc693a97fb76c2ff616f3e92d092879436f31e
bianchinmazzer <matias.bianchin@gmail.com>
2022-02-17 17:31:05 -0300

    panel administrador completo. faltan estilos

--- client/src/components/AdminProfile/EstablishmentTable.jsx
+++ client/src/components/AdminProfile/EstablishmentTable.jsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { SERVER_URL } from "../../redux/actions/actionNames";
-import { useEffect } from "react";
-import { faWindows } from "@fortawesome/free-brands-svg-icons";
 
 function EstablihsmentTable({ establishment }) {
   const userToken = useSelector((state) => state.register.userToken);
+  const [data, setData] = useState(establishment);
+
+  function handleSearch(input) {
+    switch (input.target.id) {
+      case "name":
+        if (input.target.value !== "") {
+          setData(() => {
+            return establishment.filter((item) =>
+              item.name.toLowerCase().includes(input.target.value.toLowerCase())
+            );
+          });
+        } else {
+          setData(establishment);
+        }
+        break;
+      case "cuit":
+        if (input.target.value !== "") {
+          setData(() => {
+            return establishment.filter((item) =>
+              item.cuit.includes(input.target.value)
+            );
+          });
+        } else {
+          setData(establishment);
+        }
+        break;
+      default:
+        setData(establishment);
+        break;
+    }
+  }
 
   function HandleHabilitarEst(establishmentId) {
-    // useEffect(() => {
-    console.log(establishmentId);
     const headers = {
       Authorization: `Bearer ${userToken}`,
     };
@@ -22,7 +49,6 @@ function EstablihsmentTable({ establishment }) {
       }
     );
     window.location.reload();
-    // }, [establishmentId]);
   }
 
   return (
@@ -30,8 +56,28 @@ function EstablihsmentTable({ establishment }) {
       <table className="ml-36 mt-10 table-auto border-collapse border border-slate-500 text-white">
         <thead className="bg-slate-600">
           <tr>
-            <th className="border border-slate-600 px-10">Cuit</th>
-            <th className="border border-slate-600 px-10">Nombre</th>
+            <th className="border border-slate-600 px-10">
+              Cuit
+              <div>
+                <input
+                  className="text-black"
+                  id="cuit"
+                  type="text"
+                  onChange={(e) => handleSearch(e)}
+                ></input>
+              </div>
+            </th>
+            <th className="border border-slate-600 px-10">
+              Nombre
+              <div>
+                <input
+                  className="text-black"
+                  id="name"
+                  type="text"
+                  onChange={(e) => handleSearch(e)}
+                ></input>
+              </div>
+            </th>
             <th className="border border-slate-600 px-10">
               Horario de Apertura
             </th>
@@ -42,14 +88,17 @@ function EstablihsmentTable({ establishment }) {
           </tr>
         </thead>
         <tbody className="text-center text-white">
-          {establishment?.map((est) => (
+          {data?.map((est) => (
             <tr key={est.id}>
               <td className="border border-slate-700">{est.cuit}</td>
               <td className="border border-slate-700">{est.name}</td>
               <td className="border border-slate-700">{est.timeActiveFrom}</td>
               <td className="border border-slate-700">{est.timeActiveTo}</td>
               <td className="border border-slate-700">
-                <label onClick={() => HandleHabilitarEst(est.id)}>
+                <label
+                  className="cursor-pointer"
+                  onClick={() => HandleHabilitarEst(est.id)}
+                >
                   {est.isActive ? "Habilitado" : "Deshabilitado"}
                 </label>
               </td>
--- client/src/components/AdminProfile/UsersTable.jsx
+++ client/src/components/AdminProfile/UsersTable.jsx
@@ -1,9 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { SERVER_URL } from "../../redux/actions/actionNames";
+import { useSelector } from "react-redux";
 
 function UsersTable({ users }) {
+  const userToken = useSelector((state) => state.register.userToken);
   const [usersTemp, setUsersTemp] = useState(users);
 
-  useEffect(() => {}, [usersTemp]);
+  function handleSearch(input) {
+    switch (input.target.id) {
+      case "name":
+        if (input.target.value !== "") {
+          setUsersTemp(() => {
+            return users.filter((item) =>
+              item.name.toLowerCase().includes(input.target.value.toLowerCase())
+            );
+          });
+        } else {
+          setUsersTemp(users);
+        }
+        break;
+      case "lastName":
+        if (input.target.value !== "") {
+          setUsersTemp(() => {
+            return users.filter((item) =>
+              item.lastName
+                .toLowerCase()
+                .includes(input.target.value.toLowerCase())
+            );
+          });
+        } else {
+          setUsersTemp(users);
+        }
+        break;
+      default:
+        setUsersTemp(users);
+        break;
+    }
+  }
 
   function handleFilter(e) {
     if (e.target.value !== "all") {
@@ -17,13 +51,46 @@ function UsersTable({ users }) {
     }
     console.log(usersTemp, e.target.value);
   }
+  function HandleHabilitarUs(userId) {
+    const headers = {
+      Authorization: `Bearer ${userToken}`,
+    };
+    axios.put(
+      `${SERVER_URL}/users/updateStatus`,
+      { userId },
+      {
+        headers: headers,
+      }
+    );
+    window.location.reload();
+  }
   return (
     <div>
       <table className="ml-36 mt-10 table-auto border-collapse border border-slate-500 text-white">
         <thead className="bg-slate-600">
           <tr>
-            <th className="border border-slate-600 px-10">Nombre</th>
-            <th className="border border-slate-600 px-10">Apellido</th>
+            <th className="border border-slate-600 px-10">
+              Nombre
+              <div>
+                <input
+                  className="text-black"
+                  id="name"
+                  type="text"
+                  onChange={(e) => handleSearch(e)}
+                ></input>
+              </div>
+            </th>
+            <th className="border border-slate-600 px-10">
+              Apellido
+              <div>
+                <input
+                  className="text-black"
+                  id="lastName"
+                  type="text"
+                  onChange={(e) => handleSearch(e)}
+                ></input>
+              </div>
+            </th>
             <th className="border border-slate-600 px-10">Email</th>
             <th className="border border-slate-600 px-10">Telefono</th>
             <th className="border border-slate-600 px-10">
@@ -53,7 +120,12 @@ function UsersTable({ users }) {
                 {u.hasEstablishment ? "si" : "no"}
               </td>
               <td className="border border-slate-700">
-                <input type="checkbox"></input>
+                <label
+                  className="cursor-pointer"
+                  onClick={() => HandleHabilitarUs(u.id)}
+                >
+                  {u.isActive ? "Habilitado" : "Deshabilitado"}
+                </label>
               </td>
             </tr>
           ))}
